006792671f53b538c57c30448837a64cd029951b
Emanuel Jose Zomoza <ema_z18@hotmail.com>
2024-04-25 19:02:22 -0300

    terminado

---
+++ script.js
@@ -0,0 +1,67 @@
+const resetBtn = document.getElementById('reset');
+const playBtn = document.getElementById('play');
+const timerEl = document.getElementById('timer');
+const root = document.querySelector(':root');
+
+//initial setup
+const totalSeconds = 60;
+let playing = false;
+let currentSeconds = totalSeconds;
+timerEl.innerText = formatTime(totalSeconds);
+
+// run the run() funtion every seconds
+const timerInterval = setInterval(run, 1000);
+
+//event listerners
+playBtn.addEventListener('click', () => {
+  playing = !playing;
+  playBtn.classList.toggle('play');
+  playBtn.classList.toggle('bg-green-500');
+  const playIcon = playBtn.querySelector('i');
+  playIcon.classList.toggle('fa-play');
+  playIcon.classList.toggle('fa-pause');
+})
+
+resetBtn.addEventListener('click', resetAll)
+
+// Run the timer
+function run() {
+  if(playing) {
+    currentSeconds -= 1;
+    if(currentSeconds <= 0) {
+      clearInterval(timerInterval);
+      resetAll();
+    }
+
+    timerEl.innerText = formatTime(currentSeconds);
+    root.style.setProperty('--degree', calcDeg())
+  }
+}
+
+// Calculate degress
+function calcDeg() {
+  return `${360 - (currentSeconds / totalSeconds) * 360}deg`
+}
+
+// Reset the timer
+function resetAll() {
+  playing = false;
+  playBtn.classList.remove('play')
+  playBtn.classList.remove('bg-green-500')
+  const playIcon = playBtn.querySelector('i');
+  playIcon.classList.add('fa-play');
+  playIcon.classList.remove('fa-pause');
+  currentSeconds = totalSeconds;
+  timerEl.innerText = formatTime(totalSeconds);
+  root.style.setProperty('--degree', '0deg' )
+}
+
+
+
+// format time
+function formatTime(seconds) {
+  const minutes = Math.floor(seconds / 60)
+  const newSeconds = seconds % 60;
+
+  return `${minutes.toString(). padStart(2, '0')}:${newSeconds.toString().padStart(2, '0')}`;
+}
